30f535c0fa2f131323359baae8a0ceca3ccce458
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-12-12 19:57:50 -0300

    first steps

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    /*$name = "Diego";
+    $food = "pizza";
+    $number = 2;
+    $money = 5.6;
+
+
+    $multi =  null;
+
+    $total =  $number * $money;
+
+    echo $total;*/
+
+
+    $counter = 5;
+
+    $counter-=2;
+    echo $counter
+?>
