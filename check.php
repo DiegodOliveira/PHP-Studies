bb09daa1d500caf70aba4cb0d9f4ed34e78e4c50
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-12-23 17:38:46 -0300

    incomplete checkboxes

---
+++ check.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="check.php" method=""post>
+        <input type="checkbox" name="pizza" value="Pizza">
+        Pizza <br>
+        <input type="checkbox" name="hamburger" value="Hamburger">
+        Hamburger <br>
+        <input type="checkbox" name="Hotdog" value="Hotdog">
+        Hotdog <br>
+        <input type="checkbox" name="taco" value="Taco">
+        Taco <br>
+    </form>
+</body>
+</html>
+
+<?php
+
+    if(isset($_POST["submit"])){
+
+
+        
+
+    }
+
+?>
