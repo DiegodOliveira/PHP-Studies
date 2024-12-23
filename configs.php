6cdc27efe3e98c8ba4335bfc505dafc33c986979
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-12-23 16:59:38 -0300

    new

---
+++ configs.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="configs.php" method="post">
+        <label>Username: </label>
+        <input type="text" name="username"><br>
+        <label>Password: </label>
+        <input type="text" name="password"><br>
+        <input type="submit" name="login" valeu="Log in">
+    </form>
+</body>
+</html>
+
+
+<?php
+
+    // $username = null;
+
+    // if(isset($username)){
+    //     echo "This variable is set";
+    // }else{
+    //     echo "This variable is not set";
+    // }
+
+    // if(empty($username)){
+    //     echo "This variable is empty";
+    // }else{
+    //     echo "This variable is not empty";
+    // }
+
+    // foreach($_POST as $key => $value){
+    //     echo "{$key} = {$value} <br>";
+    // }
+
+    if(isset($_POST["login"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        if(empty($username)){
+            echo "Username is missing";
+        }elseif(empty($password)){
+            echo "Password is missing";
+        }
+        else{
+            echo "Hello {$username}";
+        }
+    
+    }
+
+?>
